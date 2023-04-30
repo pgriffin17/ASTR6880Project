0cc29465d2fa975d7c48efb39b1bf235933ec980
jacob <jacob@paynepoint.com>
2023-04-30 17:07:59 -0500

    more informational spreadsheet

--- spreadsheet.py
+++ spreadsheet.py
@@ -23,10 +23,11 @@ def obsid_header_parse(obsid):
 
     infile = f'data/{obsid}/repro/acisf{obsid}_repro_evt2.fits'
     with fits.open(infile) as f:
-        exp_time = f[1].header['TSTOP'] - f[1].header['TSTART']
-        exp_time_print = round(exp_time/1000,None) #converts to integer in kiloseconds
-        obs_start = f[1].header['MJD-OBS']
-    return(round(obs_start-t_0_mjd,0),exp_time)
+        time_stop = f[1].header['TSTOP']/(24*3600) + f[1].header['MJDREF'] - t_0_mjd
+        time_start = f[1].header['TSTART']/(24*3600) + f[1].header['MJDREF'] - t_0_mjd
+        #exp_time_print = round(exp_time/1000,None) #converts to integer in kiloseconds
+        obs_date = f[1].header['MJD-OBS']
+    return(obs_date-t_0_mjd,time_start,time_stop)
 
 def epoch_header_parse(epoch):
     '''
@@ -38,23 +39,28 @@ def epoch_header_parse(epoch):
 
     infile = f'data/merge_test/epoch_{epoch+4}/merged_evt.fits'
     with fits.open(infile) as f:
-        exp_time = f[1].header['TSTOP'] - f[1].header['TSTART']
-        exp_time_print = round(exp_time/1000,None) #converts to integer in kiloseconds
-        obs_start = f[1].header['MJD-OBS']
-    return(obs_start-t_0_mjd,exp_time)
+        obs_time = f[1].header['TSTOP'] - f[1].header['TSTART']
+        obs_date = f[1].header['MJD-OBS']
+    return(obs_date-t_0_mjd,obs_time)
 
 def epoch_list_tabulator (obsid_list):
     #write the list to a csv file with Epoch number
     start_epoch = 4
     with open('obsids.csv','w',newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(['Group #','Representative T-T0 (days)','Exposure Time (ksec)','Obs IDs'])
+        writer.writerow(['Epoch','Observation Start *(days since GW170817)','Halfway Date T-T0 *','Obs End *','Exposure Time (ksec)','Obs IDs'])
         for obsid_row in obsid_list:
             epoch = start_epoch+obsid_list.index(obsid_row)
             exp_time_total = 0
-            representative_t_t0_weighted = 0
             for obsid in obsid_row:
-                [t_t0,exp_time]=obsid_header_parse(obsid)
-                representative_t_t0_weighted = (representative_t_t0_weighted*exp_time_total + t_t0*exp_time)/(exp_time_total+exp_time)
-                exp_time_total = exp_time_total+exp_time
-            writer.writerow([epoch,representative_t_t0_weighted,exp_time_total,', '.join(obsid_row)])
+                [t_t0,t_start,t_stop]=obsid_header_parse(obsid)
+                if exp_time_total == 0:
+                    ## this is the beginning of the obs
+                    epoch_start_time = t_start
+                obs_duration = t_stop - t_start
+                #representative_t_t0_weighted = (representative_t_t0_weighted*exp_time_total + t_t0*obs_time)/(exp_time_total+obs_time)
+                exp_time_total = exp_time_total+obs_duration
+            ## use the last obs end time
+            epoch_end_time = t_stop
+            halfway_date = epoch_start_time + (epoch_end_time-epoch_start_time)/(2)
+            writer.writerow([epoch,epoch_start_time, halfway_date,epoch_end_time,exp_time_total*(24*3.6),', '.join(obsid_row)])
